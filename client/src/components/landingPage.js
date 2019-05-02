400b6a262cba30e08a75a91138fd0688b8e7c41f
Abdullah Alabasi <Tech3790@gmail.com>
2019-05-01 22:53:08 -0500

    added project dependencies

---
+++ client/src/components/landingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class landingPage extends Component {
+  render() {
+    return (
+      <div>
+        Hello This is the landing page
+      </div>
+    )
+  }
+}
